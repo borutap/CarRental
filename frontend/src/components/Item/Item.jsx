3369394fa8408cc20e6fb5cee9711256438bea0c
Boruta Paweł 2 (STUD) <01151544@pw.edu.pl>
2021-12-09 16:58:31 +0100

    Car Details view

--- frontend/src/components/Item/Item.jsx
+++ frontend/src/components/Item/Item.jsx
@@ -24,62 +24,75 @@ export const Item = ({
                 <div className={styles.imgContainer}>
                     <img draggable={false} src={imageUrl} />
                 </div>
-                <div className={styles.header}>
-                    {brand + ' ' + model}
-                </div>
+                <div className={styles.header}>{brand + ' ' + model}</div>
             </div>
-            <div className={styles.expandToggle} onClick={() => setExpanded(!expanded)}>
+            <div className={styles.bottomContainer}>
+                {expanded && (
+                    <>
+                        <div className={styles.bottomCarDetails}>
+                            <VehicleDetails
+                                year={year}
+                                power={power}
+                                capacity={capacity}
+                                description={description}
+                            />
+                        </div>
+                        <div className={styles.bottomRentals}>
+                            <table>
+                                <Rental name="Rental hgw" price="1000 zł" />
+                                <Rental name="Dluzszanazwa" price="1400 zł" />
+                                <Rental name="bardzodluganazwa" price="1500 zł" />
+                                <Rental name="Rental 4" price="1200 zł" />
+                            </table>
+                            <div className={styles.rentButton}>
+                                RENT
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div>
+            <div
+                className={styles.expandToggle}
+                onClick={() => setExpanded(!expanded)}
+            >
                 {'Show ' + (expanded ? 'less' : 'more')}
-                <div className={classNames(styles.expandIcon, {[styles.expanded]: expanded})}>
-                    <ArrowIcon/>
+                <div
+                    className={classNames(styles.expandIcon, {
+                        [styles.expanded]: expanded,
+                    })}
+                >
+                    <ArrowIcon />
                 </div>
             </div>
         </div>
     );
 };
 
-/*
-            <div className={styles.headerContainer}>
-                <div className={styles.imgContainer}>
-                    <img src={imageUrl} />
-                </div>
-                <div className={styles.name}>
-                    <VehicleInfo brand={brand} model={model} />
-                </div>
-                <div className={styles.buttons}>
-                    <p className={styles.priceText}>1000zł</p>
-                    <div onClick={() => setExpanded(!expanded)}>
-                        <Button width={100} text="Car details" />
-                    </div>
-                    <Button width={100} text="Rent" />
-                </div>
-            </div>
-            <div className={styles.fullText}>
-                {expanded && (
-                    <VehicleDetails
-                        year={year}
-                        power={power}
-                        capacity={capacity}
-                        description={description}
-                    />
-                )}
-            </div>
-
-*/
-
 const VehicleDetails = ({ year, power, capacity, description }) => {
     return (
         <>
-            <p>
-                <b>Production year: {year}</b>
+            <p className={styles.mainDetails}>
+                <b>Year: </b>
+                {year}
             </p>
-            <p>
-                <b>Power: {power}</b>
+            <p className={styles.mainDetails}>
+                <b>Power: </b>
+                {power}
             </p>
-            <p>
-                <b>Capacity: {capacity}</b>
+            <p className={styles.mainDetails}>
+                <b>Capacity: </b>
+                {capacity}
             </p>
-            <p>{description}</p>
+            <p className={styles.description}>{description}</p>
         </>
+    );
+};
+
+const Rental = ({ name, price }) => {
+    return (
+        <tr>
+            <td className={styles.rentName}>{name}</td>
+            <td className={styles.rentPrice}>{price}</td>
+        </tr>  
     );
 };
