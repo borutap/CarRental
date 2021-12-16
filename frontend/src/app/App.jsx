d2a2855a3f7999ebce8569e9a6202e3e08a02d02
Paweł Boruta <01151544@pw.edu.pl>
2021-12-16 23:43:41 +0000

    Resolve conflict

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -2,22 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { MainView } from '@components/MainView/MainView';
-import { RentedView } from '@components/RentedView/RentedView';
-import { HistoryView } from '@components/HistoryView/HistoryView'
-// import { RentModal } from '../components/RentModal/RentModal';
-// import { ReturnModal } from '../components/ReturnModal/ReturnModal';
-
+import { HistoryView } from '@components/HistoryView/HistoryView';
 export const App = () => {
     return (
         <Router>
             <Switch>
                 <Route exact path="/history">
-                    <History testString="History" />
+                    <HistoryView testString="History" />
                 </Route>
                 <Route exact path="/rented">
-                    {/* Dla klienta jego wypozyczone, dla pracownika wszystkie wypozyczone */}
-                    <RentedView testString="Rented" />
-                    {/* <ReturnModal isOpen={true}/> */}
+                    <MainView testString="Rented" />
                 </Route>
                 <Route exact path="/">
                     <MainView testString="SEARCH" />
