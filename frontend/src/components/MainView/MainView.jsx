7b0f425afe848a9f33b47af001cc7cf0b797bc2f
Boruta Paweł 2 (STUD) <01151544@pw.edu.pl>
2022-01-20 04:12:03 +0100

    Remove mock responses

--- frontend/src/components/MainView/MainView.jsx
+++ frontend/src/components/MainView/MainView.jsx
@@ -6,54 +6,6 @@ import { SearchBar } from '../SearchBar/SearchBar';
 
 import styles from './MainView.module.scss';
 
-// GET /vehicles
-const mockVehiclesResponse = {
-    vehiclesCount: 3,
-    vehicles: [
-        {
-            id: '3fa85f64-5717-4562-b3fc-2c963f66afa5',
-            brand: 'Fiat',
-            model: 'Punto',
-            year: 1999,
-            enginePower: 696,
-            enginePowerType: 'string',
-            capacity: 42,
-            description:
-                'Dlugi opis hagikawoghawouighjaiguahgiuahgaiughaiughagiuahgaiughawiughaiguahgiauhg',
-        },
-        {
-            id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
-            brand: 'Aiat',
-            model: 'Szerszen',
-            year: 1969,
-            enginePower: 420,
-            enginePowerType: 'string',
-            capacity: 69,
-            description: 'string',
-        },
-        {
-            id: '3fa85f64-5717-4562-b3fc-2c963f66afa7',
-            brand: 'Niemam',
-            model: 'Pojecia',
-            year: 2069,
-            enginePower: 0,
-            enginePowerType: 'string',
-            capacity: 0,
-            description: 'string',
-        },
-    ],
-};
-
-// POST /vehicle/{id}/GetPrice response
-const mockPricesResponse = {
-  "price": 1000,
-  "currency": "zł",
-  // "currency": "string",
-  "generatedAt": "2021-12-07T19:20:37.759Z",
-  "expiredAt": "2021-12-07T19:20:37.759Z",
-  "quotaId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
-};
-
 // CUSTOM
 const mockPossibleRentals = {
     rentalsCount: 4,
