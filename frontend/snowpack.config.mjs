24688745befb4814773127e90ed9b5d93317a04d
Boruta Paweł 2 (STUD) <01151544@pw.edu.pl>
2022-01-20 14:17:31 +0100

    Save settings

--- frontend/snowpack.config.mjs
+++ frontend/snowpack.config.mjs
@@ -7,7 +7,7 @@ export default {
     DEV_API_URL: 'https://localhost:44329',
     API_URL: 'https://teamcrental.azurewebsites.net',
     DEV_GOOGLE_CLIENT: '438661257726-oqgj0dm34f9cdivvucr4evlf2ppk90je.apps.googleusercontent.com',
-    GOOGLE_CLIENT: '',
+    GOOGLE_CLIENT: '438661257726-hfaujld0s6cukc4rlhhrjofu9tafd6sf.apps.googleusercontent.com',
   },
   packageOptions: {
     polyfillNode: true
