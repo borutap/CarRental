062fd15f4bd8c4ea9bd8e8a5e6cedd6346240fb5
Boruta Paweł 2 (STUD) <01151544@pw.edu.pl>
2022-01-20 04:19:12 +0100

    Set production api url

--- frontend/snowpack.config.mjs
+++ frontend/snowpack.config.mjs
@@ -5,7 +5,7 @@
 export default {
   env: {
     DEV_API_URL: 'https://localhost:44329',
-    API_URL: '',
+    API_URL: 'https://teamcrental.azurewebsites.net',
     DEV_GOOGLE_CLIENT: '438661257726-oqgj0dm34f9cdivvucr4evlf2ppk90je.apps.googleusercontent.com',
     GOOGLE_CLIENT: '',
   },
